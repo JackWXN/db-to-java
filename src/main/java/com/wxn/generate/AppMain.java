a330231f440fb7844ef8f579e6a97b4c79611993
wuxiangnan3 <wuxiangnan3@jd.com>
2021-05-25 18:31:49 +0800

    放大界面

--- src/main/java/com/wxn/generate/AppMain.java
+++ src/main/java/com/wxn/generate/AppMain.java
@@ -125,7 +125,7 @@ public class AppMain {
 		this.frame.getContentPane().setBackground(new Color(Integer.decode("#F0F7FC")));
 		this.frame.setBackground(new Color(Integer.decode("#F0F7FB")));
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		this.frame.setSize(476, 522);
+		this.frame.setSize(576, 622);
 		int x = (int)(toolkit.getScreenSize().getWidth() - (double)this.frame.getWidth()) / 2;
 		int y = (int)(toolkit.getScreenSize().getHeight() - (double)this.frame.getHeight()) / 2;
 		this.frame.setLocation(x, y);
@@ -134,31 +134,31 @@ public class AppMain {
 		this.panel = new JPanel();
 		this.panel.setBorder(new BevelBorder(0, (Color)null, (Color)null, (Color)null, (Color)null));
 		this.panel.setBackground(new Color(Integer.decode("#D1E6F5")));
-		this.panel.setBounds(0, 34, 460, 139);
+		this.panel.setBounds(0, 34, 576, 119);
 		this.frame.getContentPane().add(this.panel);
 		this.panel.setLayout((LayoutManager)null);
 		JLabel dataUrl = new JLabel("数据url:");
 		dataUrl.setBackground(UIManager.getColor("FormattedTextField.selectionBackground"));
-		dataUrl.setBounds(10, 13, 48, 15);
+		dataUrl.setBounds(10, 12, 148, 20);
 		this.panel.add(dataUrl);
 		this.textDataUrl = new JTextField(url1);
-		this.textDataUrl.setBounds(66, 10, 374, 18);
+		this.textDataUrl.setBounds(66, 10, 474, 20);
 		this.panel.add(this.textDataUrl);
 		this.textDataUrl.setColumns(10);
 		this.userName = new JLabel("用户名:");
 		this.userName.setBackground(UIManager.getColor("FormattedTextField.selectionBackground"));
-		this.userName.setBounds(10, 41, 48, 15);
+		this.userName.setBounds(10, 46, 148, 20);
 		this.panel.add(this.userName);
 		this.TextuserName = new JTextField(user1);
 		this.TextuserName.setColumns(10);
-		this.TextuserName.setBounds(66, 38, 93, 18);
+		this.TextuserName.setBounds(66, 46, 200, 20);
 		this.panel.add(this.TextuserName);
 		this.textPassword = new JTextField(password1);
 		this.textPassword.setColumns(10);
-		this.textPassword.setBounds(66, 66, 93, 18);
+		this.textPassword.setBounds(66, 82, 200, 20);
 		this.panel.add(this.textPassword);
 		this.password = new JLabel("密    码:");
-		this.password.setBounds(10, 69, 48, 15);
+		this.password.setBounds(10, 82, 48, 20);
 		this.panel.add(this.password);
 		this.button = new JButton("测试连接");
 		this.button.addActionListener(new ActionListener() {
@@ -167,40 +167,39 @@ public class AppMain {
 				AppMain.this.jdbcConnect();
 			}
 		});
-		this.button.setBounds(10, 99, 149, 30);
+		this.button.setBounds(325, 82, 215, 20);
 		this.panel.add(this.button);
 		this.label_3 = new JLabel("数据库类型");
-		this.label_3.setBounds(207, 41, 87, 15);
+		this.label_3.setBounds(325, 46, 87, 20);
 		this.panel.add(this.label_3);
 		this.comboBox = new JComboBox();
 		this.comboBox.addItem("MySQL");
-		this.comboBox.setBounds(284, 38, 70, 21);
+		this.comboBox.setBounds(400, 46, 140, 20);
 		this.panel.add(this.comboBox);
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new BevelBorder(0, (Color)null, (Color)null, (Color)null, (Color)null));
 		panel_1.setBackground(new Color(Integer.decode("#D1E6F5")));
-		panel_1.setBounds(0, 208, 460, 90);
+		panel_1.setBounds(0, 188, 576, 90);
 		this.frame.getContentPane().add(panel_1);
 		panel_1.setLayout((LayoutManager)null);
 		JLabel label = new JLabel("输入包名:");
-		label.setBounds(10, 10, 89, 15);
+		label.setBounds(10, 9, 89, 20);
 		panel_1.add(label);
 		this.packsName = new JTextField();
-		this.packsName.setColumns(10);
-		this.packsName.setBounds(80, 10, 163, 16);
+		this.packsName.setBounds(80, 10, 263, 20);
 		panel_1.add(this.packsName);
 //		this.lblccxccxabc = new JLabel("可不填写(一级a二级b则生成com.a.b.xxx)");
 //		this.lblccxccxabc.setBounds(206, 35, 244, 15);
 //		panel_1.add(this.lblccxccxabc);
 		this.label_6 = new JLabel("输出位置:");
-		this.label_6.setBounds(10, 35, 89, 15);
+		this.label_6.setBounds(10, 39, 89, 20);
 		panel_1.add(this.label_6);
 		this.label_6_1 = new JLabel("输出目录:");
-		this.label_6_1.setBounds(10, 60, 89, 15);
+		this.label_6_1.setBounds(10, 64, 89, 20);
 		panel_1.add(this.label_6_1);
 		this.outPath = new JTextField();
 		this.outPath.setEditable(false);
-		this.outPath.setBounds(80, 60, 163, 16);
+		this.outPath.setBounds(80, 61, 263, 20);
 		panel_1.add(this.outPath);
 		this.button = new JButton("点击复制");
 		this.button.addActionListener(new ActionListener() {
@@ -211,7 +210,7 @@ public class AppMain {
 				clipboard.setContents(selection, null);
 			}
 		});
-		this.button.setBounds(256, 60, 100, 16);
+		this.button.setBounds(360, 60, 100, 20);
 		panel_1.add(this.button);
 //		this.packageName2 = new JTextField();
 //		this.packageName2.setColumns(10);
@@ -220,7 +219,7 @@ public class AppMain {
 		this.filePath = new JTextField();
 		this.filePath.setColumns(10);
 		this.filePath.setEditable(false);
-		this.filePath.setBounds(80, 35, 163, 16);
+		this.filePath.setBounds(80, 36, 263, 20);
 		panel_1.add(this.filePath);
 		this.chooseFilePath = new JButton("请选择");
 		jfc = new JFileChooser();
@@ -238,15 +237,15 @@ public class AppMain {
 				}
 			}
 		});
-		this.chooseFilePath.setBounds(256, 35, 100, 16);
+		this.chooseFilePath.setBounds(360, 35, 100, 20);
 		panel_1.add(this.chooseFilePath);
 		this.lblcom = new JLabel("必须填写(例:xx.xx)");
-		this.lblcom.setBounds(256, 10, 200, 15);
+		this.lblcom.setBounds(360, 10, 200, 20);
 		panel_1.add(this.lblcom);
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new BevelBorder(0, (Color)null, (Color)null, (Color)null, (Color)null));
 		panel_2.setBackground(new Color(Integer.decode("#D1E6F5")));
-		panel_2.setBounds(1, 340, 460, 127);
+		panel_2.setBounds(1, 320, 576, 240);
 		this.frame.getContentPane().add(panel_2);
 		panel_2.setLayout((LayoutManager)null);
 		String[] columnNames = new String[]{"表名", "主键类型", "操作"};
@@ -257,19 +256,19 @@ public class AppMain {
 				return false;
 			}
 		};
-		this.table.setBounds(1, 27, 329, 43);
+		this.table.setBounds(1, 27, 389, 123);
 		this.table.setModel(this.model);
 		//0是代表的第一列
 		TableColumn column = table.getColumnModel().getColumn(0);
 		//这个是设置列的宽度
-		column.setPreferredWidth(199);
+		column.setPreferredWidth(239);
 		column = table.getColumnModel().getColumn(1);
-		column.setPreferredWidth(80);
+		column.setPreferredWidth(100);
 		column = table.getColumnModel().getColumn(2);
 		column.setPreferredWidth(50);
 		this.table.setBorder(BorderFactory.createEtchedBorder());
 		JScrollPane scrollPane = new JScrollPane(this.table);
-		scrollPane.setBounds(0, 0, 281, 127);
+		scrollPane.setBounds(0, 0, 400, 240);
 		panel_2.add(scrollPane);
 		this.btncontroller = new JButton("生成controller");
 		this.btncontroller.addActionListener(new ActionListener() {
@@ -302,6 +301,7 @@ public class AppMain {
 						}
 					}).start();
 					(new Thread() {
+						@Override
 						public void run() {
 							Generate.genController(AppMain.this.progressBar, AppMain.this.table, AppMain.this.packsName.getText(),
 									"", AppMain.this.filePath.getText());
@@ -310,10 +310,10 @@ public class AppMain {
 				}
 			}
 		});
-		this.btncontroller.setBounds(308, 10, 132, 23);
+		this.btncontroller.setBounds(410, 10, 132, 23);
 //		panel_2.add(this.btncontroller);
 		this.btnservice = new JButton("生成service");
-		this.btnservice.setBounds(308, 54, 132, 23);
+		this.btnservice.setBounds(410, 54, 132, 23);
 //		panel_2.add(this.btnservice);
 		this.btndaoMapper = new JButton("生成逆向工程");
 		this.btndaoMapper.setToolTipText("生成Model Mapper Xml Dao");
@@ -356,14 +356,14 @@ public class AppMain {
 				}
 			}
 		});
-		this.btndaoMapper.setBounds(308, 94, 132, 23);
+		this.btndaoMapper.setBounds(410, 94, 132, 23);
 		panel_2.add(this.btndaoMapper);
 		this.label_2 = new JLabel("生成规则配置");
 		this.label_2.setBounds(20, 134, 102, 15);
 		this.frame.getContentPane().add(this.label_2);
 		this.panel_3 = new JPanel();
 		this.panel_3.setBorder(new BevelBorder(0, (Color)null, (Color)null, (Color)null, (Color)null));
-		this.panel_3.setBounds(0, 0, 460, 37);
+		this.panel_3.setBounds(0, 0, 576, 37);
 		this.panel_3.setBackground(new Color(Integer.decode("#D1E6F5")));
 		this.frame.getContentPane().add(this.panel_3);
 		this.panel_3.setLayout((LayoutManager)null);
@@ -371,13 +371,13 @@ public class AppMain {
 		this.label_1.setBounds(10, 10, 121, 15);
 		this.panel_3.add(this.label_1);
 		this.lblCreateByWxn = new JLabel("Create by wxn");
-		this.lblCreateByWxn.setBounds(302, 440, 188, 33);
+		this.lblCreateByWxn.setBounds(450, 555, 100, 33);
 		this.frame.getContentPane().add(this.lblCreateByWxn);
 		this.panel_4 = new JPanel();
 		this.panel_4.setLayout((LayoutManager)null);
 		this.panel_4.setBorder(new BevelBorder(0, (Color)null, (Color)null, (Color)null, (Color)null));
 		this.panel_4.setBackground(new Color(209, 230, 245));
-		this.panel_4.setBounds(0, 176, 460, 37);
+		this.panel_4.setBounds(0, 156, 576, 37);
 		this.frame.getContentPane().add(this.panel_4);
 		this.label_4 = new JLabel("规则配置");
 		this.label_4.setBounds(10, 10, 121, 15);
@@ -386,19 +386,19 @@ public class AppMain {
 		this.panel_5.setLayout((LayoutManager)null);
 		this.panel_5.setBorder(new BevelBorder(0, (Color)null, (Color)null, (Color)null, (Color)null));
 		this.panel_5.setBackground(new Color(209, 230, 245));
-		this.panel_5.setBounds(0, 300, 460, 37);
+		this.panel_5.setBounds(0, 280, 576, 37);
 		this.frame.getContentPane().add(this.panel_5);
 		this.label_5 = new JLabel("生成操作");
-		this.label_5.setBounds(10, 10, 121, 15);
+		this.label_5.setBounds(10, 10, 121, 20);
 		this.panel_5.add(this.label_5);
 		this.progressBar = new JProgressBar();
-		this.progressBar.setBounds(141, 10, 282, 14);
+		this.progressBar.setBounds(141, 12, 400, 18);
 		this.progressBar.setMinimum(0);
 		this.progressBar.setMaximum(100);
 		this.progressBar.setStringPainted(true);
 		this.panel_5.add(this.progressBar);
 		this.label_7 = new JLabel("进度：");
-		this.label_7.setBounds(103, 10, 45, 15);
+		this.label_7.setBounds(103, 10, 45, 20);
 		this.panel_5.add(this.label_7);
 		this.btnservice.addActionListener(new ActionListener() {
 			@Override
